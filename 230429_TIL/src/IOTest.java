f05a7588074574c1604d66677a79083b1f3fc29b
yeong <xzba13@naver.com>
2023-04-29 18:46:55 +0900

    Java IOTest

---
+++ 230429_TIL/src/IOTest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class IOTest {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        System.out.println(n1 + n2);
+    }
+}
